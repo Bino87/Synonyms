1800265e7a804227784b7533d88164671b6fe50b
Bino87 <kallenved@gmail.com>
2023-08-19 19:16:29 +0200

    add comments to main.js

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,47 +1,56 @@
-async function populateDropdownWithWords() {
-    const dropdown = document.getElementById('dropdown');
+//Populates dropdown menu with all availible words
+async function populateDropdownWithWords() {   
 
     try {
-      const response = await fetch(`${apiUrl}${getAllWords}`);
-      if (!response.ok) {
-        console.error('Error fetching words:', response);
-        return;
-      }
-      const data = await response.json();
+		//Fetch response
+		const response = await fetch(`${apiUrl}${getAllWords}`);
+		if (!response.ok) {
+			console.error('Error fetching words:', response);
+			return;
+		}		
+		//parse response message as json
+		const data = await response.json();
+		
+		// get dropdown menu object
+		const dropdown = document.getElementById('dropdown');
 
-			// Clear existing options
+		// Clear existing options
     	dropdown.innerHTML = '';
 
-			 // Add null option as the first option
+		// Add null option as the first option
     	const nullOption = document.createElement('option');
     	nullOption.value = '';
     	nullOption.textContent = 'Select a word...'; // Customize the text as needed
     	dropdown.appendChild(nullOption);
 
-      data.forEach(word => {
-        const option = document.createElement('option');
-        option.value = word.value;
-        option.textContent = word.value;
-        dropdown.appendChild(option);
-      });
-
-			console.log('Dropdown refreshed with words:', data);
+		// set each word as a dropdown menu item
+		data.forEach(word => {
+			const option = document.createElement('option');
+			option.value = word.value;
+			option.textContent = word.value;
+			dropdown.appendChild(option);
+		});	
 
     } catch (error) {
-      console.error('Network error:', error);
+		
+		console.error('Network error:', error);
     }
   }
 
+//adds new word to the system
 async function handleAddWord() {
-  const newWord = document.getElementById('inputText').value;
-  const synonym = document.getElementById('dropdown').value;
+	//get inputText and dropdown objects
+	const newWord = document.getElementById('inputText').value;
+	const synonym = document.getElementById('dropdown').value;
 
-  const addSynonymRequest = {
-    NewWord: newWord,
-    Synonym: synonym
-  };
+	//Create add synonym dto request object
+	const addSynonymRequest = {
+		NewWord: newWord,
+		Synonym: synonym
+	};
 
   try {
+	  //fetch response
     const response = await fetch(`${apiUrl}${addNewWord}`, {
       method: 'POST',
       headers: {
@@ -51,66 +60,72 @@ async function handleAddWord() {
     });
 
     if (!response.ok) {
-      console.error('Error adding new word:', response);
-      return;
+		console.error('Error adding new word:', response);
+		return;
     }
-    console.log('New word added successfully.');
+   
   } catch (error) {
-    console.error('Network error:', error);
+		console.error('Network error:', error);
 		return;
   }
 
+	//reload dropdown menu items
 	await populateDropdownWithWords();
 }
 
+//fetches synonyms of a word
 async function fetchSynonyms(word) {
   const dto = {
     Value: word
   };
 
   try {
-		console.log(`${apiUrl}${getSynonyms}`);
-    const response = await fetch(`${apiUrl}${getSynonyms}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(dto)
-    });
+	
+	const response = await fetch(`${apiUrl}${getSynonyms}`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(dto)
+	});
 
     if (!response.ok) {
-      console.error('Error fetching synonyms:', response);
-      return [];
+		console.error('Error fetching synonyms:', response);
+		return [];
     }		
-    const data = await response.json();
-		console.log(data);
+	
+	//parse response message from json
+    const data = await response.json();		
     return data || [];
+	
   } catch (error) {
     console.error('Network error:', error);
     return [];
   }
 }
 
+//renders a table of synonyms
 function renderTable(synonymsResponse) {
-	console.log("rendering table");	
-  const synonyms = synonymsResponse.map(item => item.value);
-  const columns = 4;
-  let tableHtml = '<table><tbody>';
-
-  for (let i = 0; i < synonyms.length; i += columns) {
-    tableHtml += '<tr>';
-    for (let j = i; j < i + columns && j < synonyms.length; j++) {
-      tableHtml += `<td class="td-profile">${synonyms[j]}</td>`;
-    }
-    tableHtml += '</tr>';
-  }
-
-  tableHtml += '</tbody></table>';
-  const synonymsContainer = document.getElementById('synonymsContainer');
-  synonymsContainer.innerHTML = tableHtml;
-	console.log(tableHtml);
+	
+	const synonyms = synonymsResponse.map(item => item.value);
+	const columns = 4;
+	let tableHtml = '<table><tbody>';
+	
+	for (let i = 0; i < synonyms.length; i += columns) {
+		tableHtml += '<tr>';
+		for (let j = i; j < i + columns && j < synonyms.length; j++) {
+		tableHtml += `<td class="td-profile">${synonyms[j]}</td>`;
+		}
+		tableHtml += '</tr>';
+	}
+	
+	tableHtml += '</tbody></table>';
+	
+	const synonymsContainer = document.getElementById('synonymsContainer');
+	synonymsContainer.innerHTML = tableHtml;		
 }
 
+//handles fetching of synonyms
 async function handleFetchSynonyms() {
  const word = document.getElementById('inputText').value;
   fetchSynonyms(word)
@@ -119,5 +134,5 @@ async function handleFetchSynonyms() {
     });
 }
 
-
+//populate dropdown menu with availible words at start
 populateDropdownWithWords();
